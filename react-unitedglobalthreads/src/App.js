858fc3403e1eb0ab6ccfadf890d5fe8601fa0d19
IAMMICHAELNBD <michaelgrothweiler@outlook.com>
2025-02-14 15:55:10 -0600

    Added icons to menu

--- react-unitedglobalthreads/src/App.js
+++ react-unitedglobalthreads/src/App.js
@@ -19,13 +19,19 @@ const App = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/">
+                  <i className="fas fa-home"></i> Home
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">About Us</Link>
+                <Link className="nav-link" to="/about">
+                  <i className="fas fa-info-circle"></i> About Us
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/contact">Contact</Link>
+                <Link className="nav-link" to="/contact">
+                  <i className="fas fa-envelope"></i> Contact
+                </Link>
               </li>
             </ul>
           </div>
@@ -44,3 +50,4 @@ const App = () => {
 };
 
 export default App;
+
